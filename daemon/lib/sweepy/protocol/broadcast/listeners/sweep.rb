3a7a386adc7930be4509b425654f62afe0a35cf6
Pedro Teixeira <pedro.teixeira@gmail.com>
2009-10-17 00:09:49 +0100

    daemon removing from regexp and fixed string

--- daemon/lib/sweepy/protocol/broadcast/listeners/sweep.rb
+++ daemon/lib/sweepy/protocol/broadcast/listeners/sweep.rb
@@ -37,22 +37,46 @@ module Sweepy
 
           def _sweep_fragment_regexp(path, regexp_options, source)
             puts "_sweep_fragment_regexp(#{path.inspect}, #{regexp_options.inspect}, #{source})"
-            regexp = Regexp.new(path, regexp_options)
-            puts "regexp = #{regexp.inspect}"
+            base_dir = File.expand_path(Sweepy.config['sweeping']['fragments']['base_dir'])
+            puts "base_dir: #{base_dir}"
+            matcher = Regexp.new(path, regexp_options)
+            puts "regexp = #{matcher.inspect}"
+            search_dir(base_dir) do |f|
+              if f =~ matcher
+                begin
+                  puts "(1)removing #{path}"
+                  File.delete(f)
+                rescue SystemCallError => e
+                  # If there's no cache, then there's nothing to complain about
+                end
+              end
+            end
           end
 
           def _sweep_fragment(path, source)
             puts "_sweep_fragment(#{path.inspect}, #{source})"
+            base_dir = File.expand_path(Sweepy.config['sweeping']['fragments']['base_dir'])
+            path = File.expand_path(File.join(base_dir, path), base_dir)
+            if (path =~ /#{base_dir}/) == 0
+              puts "(2)removing #{path}"
+              begin
+                File.delete(path) if File              
+              rescue SystemCallError => e
+                # If there's no cache, then there's nothing to complain about
+              end
+            else
+              puts "Security Warning: #{source} tried to remove out of path: #{path}"
+            end
           end
 
           def _sweep_page(path, source)
             puts "_sweep_page(#{path.inspect}, #{source})"
-            base_dir = File.expand_path(Sweepy.config['sweeping']['base_dir'])
+            base_dir = File.expand_path(Sweepy.config['sweeping']['pages']['base_dir'])
             path = File.expand_path(File.join(base_dir, path), base_dir)
             allowed = false
             if (path =~ /#{base_dir}/) == 0
               allowed = true
-              if allowed_paths = Sweepy.config['sweeping']['allowed_paths']
+              if allowed_paths = Sweepy.config['sweeping']['pages']['allowed_paths']
                 allowed = false
                 allowed_paths.each do |allowed_path|
                   if File.fnmatch(File.join(base_dir, allowed_path), path)
@@ -66,14 +90,29 @@ module Sweepy
               puts "Security Warning: #{source} tried to remove out of path: #{path}"
             end
             if allowed
-              puts "Removing #{path}"
-              FileUtils.remove_entry_secure(path, true)
+              puts "(3)removing #{path}"
+              File.delete(path)
             else
               puts "Security Warning: #{source} tried to remove #{path}"
             end
           end
+          
+          private
+          
+            def search_dir(dir, &callback)
+              Dir.foreach(dir) do |d|
+                next if d == "." || d == ".."
+                name = File.join(dir, d)
+                if File.directory?(name)
+                  search_dir(name, &callback)
+                else
+                  callback.call name
+                end
+              end
+            end
     
         end
+  
       end
     end
   end
