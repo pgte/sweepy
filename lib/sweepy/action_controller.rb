2f5b37fea51f9374c37cbd5f07778c2fbc2fafa0
Pedro Teixeira <pedro.teixeira@gmail.com>
2009-10-23 15:30:05 +0100

    action controller not calling old expire methods now. everything should be handled by daemon.

--- lib/sweepy/action_controller.rb
+++ lib/sweepy/action_controller.rb
@@ -17,7 +17,10 @@ module Sweepy
       def expire_page_with_sweepy(path)
         logger.debug("SWEEPY: expire_page_with_sweepy(#{path})")
         Sweepy::Commander.sweep_page_broadcast(path)
+
+        #action controller not calling old expire methods now. everything should be handled by daemon.
         #expire_page_without_sweepy(path)
+        
       end
     end
     
@@ -27,7 +30,10 @@ module Sweepy
         key = key.is_a?(Regexp) ? key : fragment_cache_key(key)
         logger.debug("SWEEPY: expire_fragment_with_sweepy(#{key.inspect}, #{options.inspect})")
         Sweepy::Commander.sweep_fragment_broadcast(key)
+
+        #action controller not calling old expire methods now. everything should be handled by daemon.
         #expire_fragment_without_sweepy(key, options)
+        
       end
 
     end
